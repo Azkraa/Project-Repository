6dd99fa4d25b3dd24d8cb99c89366e41f532c65b
Azkraa <muhammadazkraa@gmail.com>
2026-01-26 09:52:47 +0700

    Update all project files

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,34 @@ const projects = [
         link: "https://demopontify.xyz"
     },
     {
-        title: "Block 12 Creative Landing Page",
+        title: "Block 12 Creative Company Profile",
         desc: "A high-conversion landing page featuring modern UI/UX for a creative agency.",
         link: "https://azkraa.github.io/block12/"
     },
     {
-        title: "Akselera Space Agency Landing Page",
+        title: "Akselera Space Agency Company Profile",
         desc: "Futuristic space agency portal with interactive elements and smooth animations.",
         link: "https://akselera.space"
+    },
+    {
+        title: "Avery Catalog",
+        desc: "A minimalist digital catalog for fashion brands looking for a clean online presence.",
+        link: "https://azkraa.github.io/Avery-Catalog/"
+    },
+    {
+        title: "Fast Cargo Company Profile",
+        desc: "Professional company profile project designed to showcase Fast Cargo as a reliable and efficient logistics and shipping service provider.",
+        link: "https://azkraa.github.io/FastCargo/"
+    },
+    {
+        title: "Evently Landing Page",
+        desc: "Modern web landing page designed for an event ticketing platform that helps users discover, explore, and purchase tickets for upcoming events with ease.",
+        link: "" // Will trigger coming-soon.html
     },
     {
         title: "E-Commerce Dashboard",
         desc: "A real-time sales tracker built with Laravel and high-performance data handling.",
         link: "" // Will trigger coming-soon.html
-    },
-    {
-        title: "Avery Catalog",
-        desc: "A minimalist digital catalog for fashion brands looking for a clean online presence.",
-        link: "https://azkraa.github.io/Avery-Catalog/" // Will trigger coming-soon.html
     }
 ];
 
